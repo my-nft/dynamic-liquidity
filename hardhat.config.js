aad4937d251218acc2250e2774befc761d698ed5
zakarya <zakarya.chami@gmail.com>
2023-09-13 14:30:28 +0100

    position manager testing

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,10 +10,21 @@ const INFURA_API_KEY = "b6271a54103e430fbc6d2ec56ff98755";
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-const SEPOLIA_PRIVATE_KEY = "";
+const SEPOLIA_PRIVATE_KEY = "85fc564b8127eb40f2ea39bd3e3c3f6cfd8c5f89e18cad8e5101ced015504c70";
 
 module.exports = {
-  solidity: "0.8.19",
+  // solidity: "0.8.19",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+      },
+      {
+        version: "0.6.6",
+        settings: {},
+      },
+    ],
+  },
   networks: {
     sepolia: {
       url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
