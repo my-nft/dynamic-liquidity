ac1cf9e44b8be6e8daebbf22b618112b84179679
ZakaryaCH <zakarya.chami@gmail.com>
2023-09-13 16:16:18 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,7 @@ const INFURA_API_KEY = "b6271a54103e430fbc6d2ec56ff98755";
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-const SEPOLIA_PRIVATE_KEY = "85fc564b8127eb40f2ea39bd3e3c3f6cfd8c5f89e18cad8e5101ced015504c70";
+const SEPOLIA_PRIVATE_KEY = "";
 
 module.exports = {
   // solidity: "0.8.19",
